7468b5e02253a2af4ea20bd1cc6e2f0e37ab8ec8
Ishwarya <ishwaryathendral@gmail.com>
2020-07-29 21:39:35 +0530

    [IMP] This repository containstopics like Introduction,Consditionals,Functions and scopes of javascript.

--- functions/functions.js
+++ functions/functions.js
@@ -1,11 +1,12 @@
-var Day = temperature(24); // Declared a variable "Day" and the function "temperature" is called
+var Day = temperature(25); // Declared a variable "Day" 
+//dEclared the function "temperature" 
  
 function temperature(temp)
 {
 if(temp>23.5)
 {
 console.log("It is a hot day"); 
-// As the temperature is 24 degree Celsius, it is a hot day
+// According to the conditions specified above, it is a hot day
 }
 else
 {
